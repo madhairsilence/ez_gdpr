4506d3a85ac19fc858dd6fc4f23e087d744afccf
Har D <harish-kumar.dhanasekaran@rntbci.com>
2023-04-03 12:13:08 +0530

    Added more convertors

---
+++ src/main/java/com/renault/gdpr/encrypt/convertor/DateConvertor.java
@@ -0,0 +1,6 @@
+package com.renault.gdpr.encrypt.convertor;
+
+import java.util.Date;
+
+public class DateConvertor extends FieldEncryptor<Date>{
+}
---
+++ src/main/java/com/renault/gdpr/encrypt/convertor/IntegerConvertor.java
@@ -0,0 +1,4 @@
+package com.renault.gdpr.encrypt.convertor;
+
+public class IntegerConvertor extends FieldEncryptor<Integer>{
+}
